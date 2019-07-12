b9240bd307749ffe546c385340687eab1f32f48e
Danilo Sanchi <danilo.sanchi@gmail.com>
2019-07-12 16:59:09 +0200

    fix bundle config

--- src/JobBoy/Bundle/JobBoyBundle/DependencyInjection/Configuration.php
+++ src/JobBoy/Bundle/JobBoyBundle/DependencyInjection/Configuration.php
@@ -17,14 +17,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('jobboy');
+        $treeBuilder = new TreeBuilder('job_boy');
 
         if (\method_exists($treeBuilder, 'getRootNode')){
             $rootNode = $treeBuilder->getRootNode();
         }
         else {
             // BC layer for symfony/config 4.1 and older
-            $rootNode = $treeBuilder->root('jobboy');
+            $rootNode = $treeBuilder->root('job_boy');
         }
 
         $rootNode
