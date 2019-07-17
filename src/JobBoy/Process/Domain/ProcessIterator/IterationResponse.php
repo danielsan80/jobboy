c65149c3f8859500ae18e26f956b10551462008b
Danilo Sanchi <danilo.sanchi@gmail.com>
2019-07-17 13:10:24 +0200

    fix response

--- src/JobBoy/Process/Domain/ProcessIterator/IterationResponse.php
+++ src/JobBoy/Process/Domain/ProcessIterator/IterationResponse.php
@@ -8,7 +8,7 @@ class IterationResponse
     /** @var bool */
     protected $hasWorked;
 
-    public function __construct(bool $hasWorked)
+    public function __construct(bool $hasWorked = true)
     {
         $this->hasWorked = $hasWorked;
     }
