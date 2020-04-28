0add9b88bb6245c730970e92c9e965edf51c6de6
Danilo Sanchi <danilo.sanchi@gmail.com>
2020-04-28 15:37:06 +0200

    namespace for locks

--- src/JobBoy/Process/Domain/Lock/Infrastructure/Symfony/LockFactory.php
+++ src/JobBoy/Process/Domain/Lock/Infrastructure/Symfony/LockFactory.php
@@ -4,30 +4,54 @@ namespace JobBoy\Process\Domain\Lock\Infrastructure\Symfony;
 
 use JobBoy\Process\Domain\Lock\LockFactoryInterface;
 use JobBoy\Process\Domain\Lock\LockInterface;
+use JobBoy\Process\Domain\Lock\LockSpace;
 use Symfony\Component\Lock\Factory;
 use Symfony\Component\Lock\Store\FlockStore;
 
 class LockFactory implements LockFactoryInterface
 {
+    const LOCKS = '/locks';
 
     /** @var string|null */
     protected $locksDir;
+    /** @var boolean */
+    protected $locksDirExists = false;
+
+    /** @var LockSpace */
+    protected $space;
 
     /** @var Factory|null */
     protected $factory;
 
-    public function __construct($locksDir = null)
+    public function __construct($locksDir = null, ?LockSpace $space = null)
     {
         if (!$locksDir) {
-            $locksDir = sys_get_temp_dir();
+            $locksDir = sys_get_temp_dir().self::LOCKS;
+        }
+
+        if (!$space) {
+            $space = new LockSpace();
         }
 
         $this->locksDir = $locksDir;
+        $this->space = $space;
+    }
+
+    protected function ensureLocksDirExists(): void
+    {
+        if ($this->locksDirExists){
+            return;
+        }
+        if (!file_exists($this->locksDir)) {
+            mkdir($this->locksDir,0777, true);
+        }
+        $this->locksDirExists = true;
     }
 
     protected function factory(): Factory
     {
         if (!$this->factory) {
+            $this->ensureLocksDirExists();
             $store = new FlockStore($this->locksDir);
 
             $factory = new Factory($store);
@@ -40,7 +64,7 @@ class LockFactory implements LockFactoryInterface
     public function create(string $name): LockInterface
     {
 
-        $lock = $this->factory()->createLock($name);
+        $lock = $this->factory()->createLock($this->space.'.'.$name);
 
         return new Lock($lock);
     }
---
+++ src/JobBoy/Process/Domain/Lock/LockSpace.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace JobBoy\Process\Domain\Lock;
+
+class LockSpace
+{
+    /** @var string */
+    protected $space;
+
+    /** @var string|null */
+    protected $hash;
+
+
+    public function __construct(string $space = null)
+    {
+        if (!$space) {
+            $space = __DIR__;
+        }
+        $this->space = $space;
+    }
+
+    public function hash(): string
+    {
+        if ($this->hash) {
+            return $this->hash;
+        }
+        $this->hash = sha1($this->space);
+
+        return $this->hash;
+    }
+
+    public function __toString()
+    {
+        return $this->hash();
+    }
+
+}
